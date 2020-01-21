2044726f26a4dd8b798d6251f57eebc33ae4fa40
Filipe Marques <filipi.marques@gmail.com>
2020-01-21 13:30:44 -0300

    add rotation fnc

---
+++ test.js
@@ -0,0 +1,17 @@
+let array = [1, 2, 3, 4, 5]
+
+
+function rotLeft() {
+  array.reverse()
+  console.log(array)
+}
+
+
+function rotation(rep) {
+  while (!(rep === 0)) {
+    rotLeft()
+    rep--
+  }
+}
+
+rotation(1000)
